5de33eeaa1cdec9acca94439082595b5c5cf130a
smart-cat-1 <jhy1750883993@gmail.com>
2026-04-23 22:04:56 +0000

    FIX

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-const theme = localStorage.getItem('theme');
-if (theme) {
-  document.body.setAttribute('data-theme', theme);
-} else {
-  localStorage.setItem('theme', 'dark');
-}
+localStorage.removeItem('theme');
+
+document.body.setAttribute('data-theme', 'light');
+document.getElementById('sun_icon').style.display = 'none';
+document.getElementById('moon_icon').style.display = 'block';
 
 function toggleTheme() {
   const sunIcon = document.getElementById('sun_icon');
   const moonIcon = document.getElementById('moon_icon');
   const body = document.body;
-  const theme = body.getAttribute('data-theme');
-  body.setAttribute('data-theme', theme === 'dark' ? 'light' : 'dark');
-  if (theme === 'dark') {
+  const currentTheme = body.getAttribute('data-theme');
+
+  const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+  body.setAttribute('data-theme', newTheme);
+
+  if (newTheme === 'dark') {
     sunIcon.style.display = 'block';
     moonIcon.style.display = 'none';
   } else {
